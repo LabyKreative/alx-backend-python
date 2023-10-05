642e4e6f9d93b660f242394b7bcc799b067b04a3
LabKreative <labykreative@yahoo.com>
2023-10-05 20:02:18 +0000

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" a function add that takes a float a and a float b
+""" A function add that takes a float a and a float b
 as arguments and returns their sum as a float.
 """
 
