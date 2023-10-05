224d1fb1935b7b646109c15d014e6b4f85181681
LabKreative <labykreative@yahoo.com>
2023-10-05 20:37:27 +0000

    Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-type-annotated function floor which takes a float n as argument and returns the floor of the float.
+ function floor which takes a float n as argument.
 '''
 
 
