a867466c7f15db9aade5860431d5cbbe2f5b7897
LabKreative <labykreative@yahoo.com>
2023-10-05 21:43:03 +0000

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
-'''
-Type Checking
-'''
+"""Type Checking."""
 from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    '''
-    Creates multiple copies of items in a tuple.
-    '''
+    """Creates multiple copies of items in a tuple."""
     zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
     ]
     return zoomed_in
-
