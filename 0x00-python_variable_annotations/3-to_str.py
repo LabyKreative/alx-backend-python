e87456d692f237a2aa3cfc612eb65c42638aa366
LabKreative <labykreative@yahoo.com>
2023-10-05 20:35:49 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+ function to_str that takes a float n as arg
+'''
+
+
+def to_str(n: float) -> str:
+    '''Converts a floating-point number to a string.
+    '''
+    return str(n)
